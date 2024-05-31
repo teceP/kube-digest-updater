eb3dd131ba986f827c033e19b8eedd0ad26d48c6
Mario Teklic <m.teklic@akkusys.de>
2024-05-31 17:24:12 +0200

    .

--- bp.bat
+++
@@ -1,3 +0,0 @@
-docker build -t pp4l1660.c1.de1.container-registry.ovh.net/library/image-updater:1.0.0-SNAPSHOT .
-sleep 2s
-docker push pp4l1660.c1.de1.container-registry.ovh.net/library/image-updater:1.0.0-SNAPSHOT
